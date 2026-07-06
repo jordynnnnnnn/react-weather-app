1f7145d998533fabd811fd258539e89d383b0fcb
jordynnnnnnn <jordyn.pilcher@hotmail.com>
2026-07-06 14:05:25 +0930

    Added Github repository URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jordyn Pilcher and is {""}
+        <a
+          href="https://github.com/jordynnnnnnn/react-weather-app"
+          target="_blank"
+        >
+          open-sourced in Github
+        </a>
+      </footer>
+    </div>
+  );
 }
