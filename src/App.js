dd1e28b97904bf732b1f13e46cf302e31552015f
jordynnnnnnn <jordyn.pilcher@hotmail.com>
2026-07-15 11:09:24 +0930

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Jordyn Pilcher and is {""}
@@ -15,6 +16,7 @@ export default function App() {
           open-sourced in Github
         </a>
       </footer>
+      </div>
     </div>
   );
 }
