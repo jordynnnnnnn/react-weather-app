7f778891b6503ea82451cecec68c6e3621065854
jordynnnnnnn <jordyn.pilcher@hotmail.com>
2026-07-21 14:57:44 +0930

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather defaultCity="Tokyo" />
-      <footer>
-        This project was coded by Jordyn Pilcher and is {""}
-        <a
-          href="https://github.com/jordynnnnnnn/react-weather-app"
-          target="_blank"
-        >
-          open-sourced in Github
-        </a>
-      </footer>
+        <Weather defaultCity="Tokyo" />
+        <footer>
+          This project was coded by Jordyn Pilcher and is {""}
+          <a
+            href="https://github.com/jordynnnnnnn/react-weather-app"
+            target="_blank"
+          >
+            open-sourced in Github
+          </a>
+        </footer>
       </div>
     </div>
   );
